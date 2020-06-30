81349761d769bcdf4594c44a17cad081bf89ec35
MasashinoMikami <j1234572923@gmail.com>
2020-06-30 11:11:22 +0900

    スタッフ追加

---
+++ staff.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"?>
+        <title>スタッフ</title>　
+    </head>
+    <body>
+        スタッフ追加<br/>
+        <br/>
+        <form method="post"action="staff_check.php">
+            スタッフ名入力。<br/>
+            <input type="text"name="name"style="width:200px"><br/>
+            パスワード入力。<br/>
+            <input type="password"name="pass"style="width:200px"><br/>
+            <br/>
+            <input type="button"
+
+    </body>
+</html> 
